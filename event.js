2e4545273e65f5ef464e8b325438cec5135855c2
Kristin Caron <krissycaron@gmail.com>
2017-03-06 13:04:37 -0600

    variables added

---
+++ event.js
@@ -0,0 +1,24 @@
+
+// Note: Output target is the output-target element.
+// When any section is clicked the output target text should be "You clicked on the {text of the section} section"
+// When the mouse is over the h1 tag, the output element should contain the text "You moved your mouse over the header".
+// When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
+// When you type characters into the input field, the output element should mirror the text in the input field.
+// When you click the "Add color" button, the guinea-pig element's text color should change to blue.
+// When you click the "Hulkify" button, the guinea-pig element's font size should become much larger.
+// When you click the "Capture it" button, the guinea-pig element should have a border added to it.
+// When you click the "Rounded" button, the guinea-pig element's border should become rounded.
+// The first section's text should be bold.
+// The last section's text should be bold and italicized.
+// Make the buttons stop appearing next to each other as inline elements. Change them into block elements.
+var outputElement = document.getElementByID("output-target");
+var articleElement = document.getElementByClassName("article-section");
+var headingColor = document.getElementByID("page-title");
+var addColorBtn = document.getElementByID("add-color");
+var addHulkifyBtn = document.getElementByID("make-large");
+var addBorderBtn = document.getElementByID("add-border"); 
+var addRoundingBtn = document.getElementByID("add-rounding");
+
+
+
+
